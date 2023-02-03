3f427eb226a0aba6013cbabd4e6d3d168e193ead
Twinkle Popat <twinkle.p@ahduni.edu.in>
2023-02-03 14:32:03 +0530

    display weekday

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import  express  from "express";
-import bodyParser from "body-parser";
+const express = require("express");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
@@ -8,13 +9,34 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) => {
   var today = new Date();
   var currentDay = today.getDay();
-  if(currentDay === 6 || currentDay === 0){
-    res.send("Yay! It's the weekend!");
+  var day = "";
+  switch(currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
+
   }
-  else{
-    res.send("Boo! I have to work!");
-  }
-  res.send("Hello World!");
+  res.render("list", {dayType: day});
 });
 
 app.listen(3000, () => {
